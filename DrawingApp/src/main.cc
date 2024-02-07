7850b4d97953ef5b49e8b8f14235d58371fa28f4
Maksym Ivanov <sinnwidriger@gmail.com>
2024-02-07 07:30:44 +0100

    Readjust "DrawingApp" project structure.

--- DrawingApp/src/main.cc
+++ DrawingApp/src/main.cc
@@ -1,5 +1,5 @@
 #include "stdafx.h"
-#include "drawing_app.h"
+#include "drawing_app/drawing_app.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ PSTR szCmdLine, _In_ int iCmdShow)
 {
